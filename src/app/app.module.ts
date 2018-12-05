f8cc5a160b5c85d9f59e4fab9c48772108676ab7
Shyam Patel <sam_r_patel@hotmail.com>
2018-12-05 03:37:21 -0500

    Added components to shopping list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-// Import the header component, because the file runs from here
-import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// Import the header component, because the file runs from here
+import { HeaderComponent } from './header/header.component';
 // Import all the components here
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
