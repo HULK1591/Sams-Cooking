73dd375def13748c6b57f96cdc8365565ddc1962
Shyam Patel <sam_r_patel@hotmail.com>
2018-12-05 02:23:41 -0500

    Added all the components for Recipes and Shopping list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,28 @@
+// Import the header component, because the file runs from here
+import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// Import all the components here
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShopListComponent } from './shop-list/shop-list.component';
+import { ShopEditComponent } from './shop-list/shop-edit/shop-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // Header component initilized
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShopListComponent,
+    ShopEditComponent
   ],
   imports: [
     BrowserModule
