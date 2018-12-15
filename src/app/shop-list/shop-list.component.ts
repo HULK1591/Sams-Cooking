6db7a9d6a7390359d10a35c70a65926c538c433b
Shyam Patel <sam_r_patel@hotmail.com>
2018-12-15 01:10:16 -0500

    Ingredients file for sharing accross the app. Added shop-edit btns

--- src/app/shop-list/shop-list.component.ts
+++ src/app/shop-list/shop-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+// Imports from ingredients.model file
+import { Ingredient } from './../shared/ingredient.model';
+
 @Component({
   selector: 'app-shop-list',
   templateUrl: './shop-list.component.html',
   styleUrls: ['./shop-list.component.css']
 })
 export class ShopListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 15)
+  ];
 
   constructor() { }
 
